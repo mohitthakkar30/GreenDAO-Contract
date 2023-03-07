4efe96d833cacd70dec79a6fb9dcd6e67000638b
Mohit Thakkar <45704711+mohitthakkar30@users.noreply.github.com>
2023-03-07 09:30:58 +0530

    LFG

---
+++ GreenToken.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract GreenToken is ERC20Votes {
+    constructor() ERC20("Green Token", "GREEN") ERC20Permit("Green Token") {}
+
+    function mint() external {
+        require(balanceOf(msg.sender) == 0, "Max minting supply reached");
+        _mint(payable(msg.sender), 1);
+    }
+
+    // The functions below are overrides required by Solidity.
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount) internal override(ERC20Votes) {
+        super._mint(to, amount);
+    }
+
+    function _burn(
+        address account,
+        uint256 amount
+    ) internal override(ERC20Votes) {
+        super._burn(account, amount);
+    }
+}
